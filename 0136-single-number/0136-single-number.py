147e5994839693191d39d57476b5f03505da9ef7
Ashutosh Pandey <119648763+ashutoshpandey1785@users.noreply.github.com>
2024-09-24 15:41:41 +0530

    Time: 101 ms (93.1%), Space: 19.1 MB (55.09%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,8 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        co=0
+        ans=0
         for i in nums:
-            ans=nums.count(i)
-            if ans==1:
-                co+=i
-        return co
+            ans=ans^i
+        return ans
